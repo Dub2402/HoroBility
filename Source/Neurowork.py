734143ac50a1b5273e4b0a24798be4e4fad57c21
Irina Dub <109979502+kostevich@users.noreply.github.com>
2024-11-06 22:38:47 +0300

    Add: check language

--- Source/Neurowork.py
+++ Source/Neurowork.py
@@ -1,12 +1,25 @@
 from g4f.client import Client
 from dublib.Polyglot import Markdown
 from Source.Functions import GetTodayDate
-from langdetect import detect
 
-import json
 import random
 
 class Neurwork:
+
+	def __match_rus(self, character, alphabet=set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')):
+		
+		return character.lower() in alphabet
+	
+	def __IsTextRussian(self, text):
+		IsRussian = True 
+
+		for Character in text:
+			if Character.isalpha() and not self.__match_rus(Character):
+				IsRussian = False
+				break
+
+		return IsRussian
+		
 
 	def __init__(self) -> None:
 			self.__Client = Client()
@@ -19,21 +32,19 @@ class Neurwork:
 		Today = Markdown(GetTodayDate()).escaped_text
 
 		while not Result:
-			Request = f"Сгенерируй {first_zodiak} и {second_zodiak} совместимость на {Percent}% только на сегодня. Не добавляй в текст процент совместимости!!!"
+			Request = f"Представь себя гадалкой. Составь совместимость {first_zodiak} и {second_zodiak} на сегодняшний день, если известно, что их совместимость {Percent}%. Не добавляй в текст процент совместимости!!!"
 			Response = self.__Client.chat.completions.create(model = "gpt-4", messages = [{"role": "user", "content": Request}])
 			MainText = Markdown(Response.choices[0].message.content).escaped_text
+			print(Result)
 
-			if len(MainText) > 10:
-				Result = True
+			if self.__IsTextRussian(MainText) and "терапевт" not in MainText and "психолог" not in MainText: Result = True
 
-			try:
-				dictData = json.loads(Response.choices[0].message.content)
-				Result = False
-			except json.decoder.JSONDecodeError:
-				detect_language = detect(MainText)
-				if detect_language != "ru": Result = False
-				if Response.choices[0].message.content == "No message received" or Response.choices[0].message.content == "Request ended with status code 404": Result = False
-			
 		Text = Title + "\n" + f"*Совместимость на {Today}*" + "\n\n" + MainText + "\n\n" + f"*💠 Совместимость на сегодня __\\- {Percent}%__*"
+		print(Text)
 		
 		return Text
+
+
+
+
+
